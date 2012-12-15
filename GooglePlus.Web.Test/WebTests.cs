093688896ec6228c1b379e0371a7df1140053376
Dmitrijs <dmitrijs.v@gmail.com>
2012-12-15 13:43:45 +0200

    WebDriver tests, hopefully fixed some references

--- GooglePlus.Web.Test/WebTests.cs
+++ GooglePlus.Web.Test/WebTests.cs
@@ -42,6 +42,49 @@ namespace GooglePlus.Web.Test
             Assert.AreEqual(userInfo.Item1, usernameField.GetAttribute("value"));
         }
 
+        [TestMethod]
+        public void UserAdditionalDataOnRegistrationTest()
+        {
+            var userInfo = GetUserNameAndPassword();
+
+            string birthday = new DateTime(1990, 1, 13).ToShortDateString();
+            const string education = "LU, Datorikas fakultāte";
+            const string location = "Riga";
+
+            Register(userInfo.Item1, userInfo.Item2, birthday, education, location);
+
+            Assert.AreEqual(birthday, DateTime.Parse(GetValue(webDriver, By.Id("BirthDay"))).ToShortDateString());
+            Assert.AreEqual(education, GetValue(webDriver, By.Id("Education")));
+            Assert.AreEqual(location, GetValue(webDriver, By.Id("Location")));
+        }
+
+        [TestMethod]
+        public void UserAdditionalDataChangeTest()
+        {
+            var userInfo = GetUserNameAndPassword();
+
+            string birthday = new DateTime(1990, 1, 13).ToShortDateString();
+            const string education = "LU, Datorikas fakultāte";
+            const string location = "Riga";
+
+            Register(userInfo.Item1, userInfo.Item2);
+
+            webDriver.FindElement(By.Id("BirthDay")).SendKeys(birthday);
+            webDriver.FindElement(By.Id("Education")).SendKeys(education);
+            webDriver.FindElement(By.Id("Location")).SendKeys(location);
+
+            webDriver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+
+            Assert.AreEqual(birthday, birthday, DateTime.Parse(GetValue(webDriver, By.Id("BirthDay"))).ToShortDateString());
+            Assert.AreEqual(education, GetValue(webDriver, By.Id("Education")));
+            Assert.AreEqual(location, GetValue(webDriver, By.Id("Location")));
+        }
+
+        private string GetValue(IWebDriver driver, By elementLocator)
+        {
+            return driver.FindElement(elementLocator).GetAttribute("value");
+        }
+
         private Tuple<string, string> GetUserNameAndPassword()
         {
             var name = Guid.NewGuid().ToString().Replace("-", String.Empty);
